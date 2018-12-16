a092b027f258af29072f4aa9b872cf90ac70adbb
novandev <donovan.adams@students.makeschool.com>
2018-12-15 22:45:42 -0800

    adding more to a repo

---
+++ generators.py
@@ -0,0 +1,25 @@
+# This is a warm up exercise on generators
+
+# A simple generator function
+def my_gen():
+    n = 1
+    print('This is printed first')
+    # Generator function contains yield statements
+    yield n
+
+    n += 1
+    print('This is printed second')
+    yield n
+
+    n += 1
+    print('This is printed at last')
+    yield n
+
+
+
+a = my_gen()
+
+next(a)
+next(a)
+next(a)
+next(a)
